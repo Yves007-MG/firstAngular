133b9cee1639a74032feb07790c3261d72a6b392
yves <ramarojaona.yves@gmail.com>
2022-08-22 14:01:44 +0300

    appareil components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   isAuth = false ;
+  appareilOne = 'Machine a laver';
+  appareilTwo = 'Television';
+  appareilThree = 'Ordinateur';
   constructor(){
     setTimeout(
       () =>{
--- src/app/appareil/appareil.component.ts
+++ src/app/appareil/appareil.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-appareil',
@@ -7,8 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppareilComponent implements OnInit {
 
-  appareilName   = 'Machine à Laver'
-  appareilStatus = 'eteint';
+  @Input() 
+  appareilName: string | undefined ;
+  @Input()
+  appareilStatus: string | undefined ;
 
   constructor() { }
 
